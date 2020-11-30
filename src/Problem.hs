77cef18add97d77ac4e4e3ee85b9ef91f9c25913
Will Leyton <wleylop22@gmail.com>
2020-11-30 07:57:38 -0500

    Add problem

---
+++ src/Problem.hs
@@ -0,0 +1,14 @@
+module Problem where
+
+data Expr = 
+  Val Int | 
+  Add Expr Expr
+  
+eval :: Expr -> Int
+eval (Val i) = i
+eval (Add x y) = eval x + eval y
+
+myExpr :: Expr
+myExpr = Add (Val 1) (Add (Val 10) (Val 2))
+
+-- Problem: If we add new types of Expr, we must update all our functions.
